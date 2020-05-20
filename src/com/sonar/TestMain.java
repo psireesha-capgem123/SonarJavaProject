d250f166d8560f42e756bfccb40cad352935034b
psireesha-capgeminidotcom <sireesha.pappala@capgemini.com>
2020-05-20 17:33:33 +0530

    Test Java Project

---
+++ src/com/sonar/TestMain.java
@@ -0,0 +1,11 @@
+package com.sonar;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		System.out.println(" test project :: " + (a + b));
+	}
+
+}
