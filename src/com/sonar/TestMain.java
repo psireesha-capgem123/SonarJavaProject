9b48710026e4b6e34546c72506d7483b151bed53
psireesha-capgeminidotcom <sireesha.pappala@capgemini.com>
2020-05-20 17:36:18 +0530

    TestMain.Java change

--- src/com/sonar/TestMain.java
+++ src/com/sonar/TestMain.java
@@ -5,7 +5,8 @@ public class TestMain {
 	public static void main(String[] args) {
 		int a = 10;
 		int b = 20;
-		
+		int c = (a+b);
+		System.out.println(c);
 		System.out.println(" test project :: " + (a + b));
 		
 	}
